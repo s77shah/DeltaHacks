30068aaa802e9d1d945953aa6d2b7b9971030914
Duck_luv_pie <eyoon06@gmail.com>
2025-01-11 16:31:11 -0500

    Initial commit

--- deltahacks/ContentView.swift
+++ deltahacks/ContentView.swift
@@ -1,24 +1,138 @@
-//
-//  ContentView.swift
-//  deltahacks
-//
-//  Created by Eric on 2025-01-11.
-//
-
 import SwiftUI
+import Auth0
+
 
 struct ContentView: View {
-    var body: some View {
+  
+  @State private var isAuthenticated = false
+  @State var userProfile = Profile.empty
+  
+  var body: some View {
+      
+    if isAuthenticated {
+      VStack {
+
+        Text("You’re logged in!")
+          .modifier(TitleStyle())
+  
+        UserImage(urlString: userProfile.picture)
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+          Text("Name: \(userProfile.name)")
+          Text("Email: \(userProfile.email)")
         }
         .padding()
+        
+        Button("Log out") {
+          logout()
+        }
+        .buttonStyle(MyButtonStyle())
+        
+      } // VStack
+    
+    } else {
+      
+      VStack {
+        
+        Text("Welcome to Proj")
+          .modifier(TitleStyle())
+        
+        Button("Log in") {
+          login()
+        }
+        .buttonStyle(MyButtonStyle())
+        
+      } // VStack
+      
     }
+    
+  }
+  
+  
+  struct UserImage: View {
+    var urlString: String
+    var body: some View {
+      AsyncImage(url: URL(string: urlString)) { image in
+        image
+          .frame(maxWidth: 128)
+      } placeholder: {
+        Image(systemName: "person.circle.fill")
+          .resizable()
+          .scaledToFit()
+          .frame(maxWidth: 128)
+          .foregroundColor(.blue)
+          .opacity(0.5)
+      }
+      .padding(40)
+    }
+  }
+
+  struct TitleStyle: ViewModifier {
+    let titleFontBold = Font.title.weight(.bold)
+    let navyBlue = Color(red: 0, green: 0, blue: 0.5)
+    
+    func body(content: Content) -> some View {
+      content
+        .font(titleFontBold)
+        .foregroundColor(navyBlue)
+        .padding()
+    }
+  }
+  
+  struct MyButtonStyle: ButtonStyle {
+    let navyBlue = Color(red: 0, green: 0, blue: 0.5)
+    
+    func makeBody(configuration: Configuration) -> some View {
+      configuration.label
+        .padding()
+        .background(navyBlue)
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+    }
+  }
+  
 }
 
-#Preview {
-    ContentView()
+
+extension ContentView {
+  
+  func login() {
+    Auth0
+      .webAuth()
+      .start { result in
+        switch result {
+          case .failure(let error):
+            print("Failed with: \(error)")
+
+          case .success(let credentials):
+            self.isAuthenticated = true
+            self.userProfile = Profile.from(credentials.idToken)
+            print("Credentials: \(credentials)")
+            print("ID token: \(credentials.idToken)")
+        }
+      }
+  }
+  
+  func logout() {
+    Auth0
+      .webAuth()
+      .clearSession { result in
+        switch result {
+          case .success:
+            self.isAuthenticated = false
+            self.userProfile = Profile.empty
+
+          case .failure(let error):
+            print("Failed with: \(error)")
+        }
+      }
+  }
+  
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
