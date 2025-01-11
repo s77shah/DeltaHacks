6597df1604a8a04a864ed9ee70ab585f9db842a5
Duck_luv_pie <eyoon06@gmail.com>
2025-01-11 15:22:12 -0500

    Initial Commit

---
+++ deltahacks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  deltahacks
+//
+//  Created by Eric on 2025-01-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ deltahacks/deltahacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  deltahacksApp.swift
+//  deltahacks
+//
+//  Created by Eric on 2025-01-11.
+//
+
+import SwiftUI
+
+@main
+struct deltahacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
